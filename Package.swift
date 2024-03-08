c33c121b01d6325763b0725c564dfdf8d02b0eee
julianko13 <30486126+julianko13@users.noreply.github.com>
2024-03-08 15:26:47 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -30,7 +30,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "OctopusKit",
-            dependencies: ["OctopusCore"],
+            //dependencies: ["OctopusCore"],
+            .dependencies: [.product(name: "OctopusCore", package:"octopuscore"]
             exclude: [
                 "Apple API Extensions/SwiftUI/OctopusUI.md"],
             resources: [
